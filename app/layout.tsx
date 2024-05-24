58a5050be2fc420f4a463f0a2141865c685a58b8
kabraliev2005 <kabraliev2005@gmail.com>
2024-05-24 19:01:18 +0500

    seo optim...

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,15 +9,17 @@ export const metadata: Metadata = {
     default: "Muhiddindev.uz",
     template: "%s | Muhiddindev.uz",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description:
+    "Hello friend, my name is Muhiddin and I am from Uzbekistan ✔ +998905650213 for contact @black_hole_225 for telegram",
   openGraph: {
-    title: "Muhiddindev.uz",
-    description: "Co-founder of unkey.dev and founder of planetfall.io",
+    title: "Muhiddindev",
+    description:
+      "Hello friend, my name is Muhiddin and I am from Uzbekistan ✔ +998905650213 for contact @black_hole_225 for telegram",
     url: "https://muhddindev.uz",
-    siteName: "muhiddindev.uz",
+    siteName: "Muhiddindev",
     images: [
       {
-        url: "https://muhiddindev.uz/og.png",
+        url: "https://avatars.githubusercontent.com/u/88494561?s=400&u=c5ca80f7ea816a960094d012395a0e5cce8da3c5&v=4",
         width: 1920,
         height: 1080,
       },
@@ -35,10 +37,6 @@ export const metadata: Metadata = {
       "max-image-preview": "large",
       "max-snippet": -1,
     },
-  },
-  twitter: {
-    title: "Chronark",
-    card: "summary_large_image",
   },
   icons: {
     shortcut: "/favicon.png",
