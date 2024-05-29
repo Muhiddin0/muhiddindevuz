bc358c220e765c74009ad0c9133e8afe52115924
kabraliev2005 <kabraliev2005@gmail.com>
2024-05-29 13:26:26 +0500

    fix

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -6,81 +6,88 @@ import { Header } from "./header";
 import { instance } from "@/app/services";
 import "./mdx.css";
 import { MODERATOR_ID } from "@/app/lib/config";
+import axios from "axios";
 
 export const revalidate = 60;
 
 type Props = {
-  params: {
-    slug: string;
-  };
+    params: {
+        slug: string;
+    };
 };
 
-// export async function generateMetadata({ params }: Props) {
-//   let {
-//     data: project,
-//   }: {
-//     data: Project;
-//   } = await instance.get(`/moderator/${MODERATOR_ID}/projects/${params.slug}`);
+export async function generateMetadata({ params }: Props) {
+    let {
+        data: project,
+    }: {
+        data: Project;
+    } = await instance.get(
+        `/moderator/${MODERATOR_ID}/projects/${params.slug}`
+    );
 
-//   return {
-//     title: project.title,
-//     description: project.description,
-//     url: "https://muhddindev.uz/projects/" + params.slug,
-//     images: project.images.map((image) => ({
-//       url: image.image,
-//       width: 1920,
-//       height: 1080,
-//     })),
-//     openGraph: {
-//       title: project.title,
-//       description: project.description,
-//       url: "https://muhddindev.uz/projects/" + params.slug,
-//       images: project.images.map((image) => ({
-//         url: image.image,
-//         width: 1920,
-//         height: 1080,
-//       })),
-//     },
-//   };
-// }
+    return {
+        title: project.title,
+        description: project.description,
+        url: "https://muhddindev.uz/projects/" + params.slug,
+        images: project.images.map((image) => ({
+            url: image.image,
+            width: 1920,
+            height: 1080,
+        })),
+        openGraph: {
+            title: project.title,
+            description: project.description,
+            url: "https://muhddindev.uz/projects/" + params.slug,
+            images: project.images.map((image) => ({
+                url: image.image,
+                width: 1920,
+                height: 1080,
+            })),
+        },
+    };
+}
 
 export default async function PostPage({ params }: Props) {
-  try {
-    await instance.post(
-      `/moderator/${MODERATOR_ID}/projects/${params.slug}/increment`
-    );
+    try {
+        let { data } = await axios.get("https://api.ipify.org/?format=json");
 
-    let {
-      data: project,
-    }: {
-      data: Project;
-    } = await instance.get(
-      `/moderator/${MODERATOR_ID}/projects/${params.slug}`
-    );
+        await instance.post(
+            `/moderator/${MODERATOR_ID}/projects/${params.slug}/increment/${data.ip}`
+        );
 
-    return (
-      <div className="bg-zinc-50 min-h-screen">
-        <Header project={project} />
-        <div className="container mx-auto">
-          {project.videos.map((video) => (
-            <iframe
-              src={video.video_url}
-              title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              className="w-[80%] h-[150px] min-[291px]:h-[200px] min-[500px]:h-[300px] min-[600px]:h-[400px] xl:h-[500px] mx-auto my-5"
-              // @ts-ignore
-              allowfullscreen
-            ></iframe>
-          ))}
-        </div>
+        let {
+            data: project,
+        }: {
+            data: Project;
+        } = await instance.get(
+            `/moderator/${MODERATOR_ID}/projects/${params.slug}`
+        );
 
-        <Gallery title={project.title} images={project.images} />
-      </div>
-    );
-  } catch (error) {
-    console.log(error);
-    console.log(`/moderator/${MODERATOR_ID}/projects/${params.slug}/increment`);
+        return (
+            <div className="bg-zinc-50 min-h-screen">
+                <Header project={project} />
+                <div className="container mx-auto">
+                    {project.videos.map((video) => (
+                        <iframe
+                            src={video.video_url}
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            className="w-[80%] h-[150px] min-[291px]:h-[200px] min-[500px]:h-[300px] min-[600px]:h-[400px] xl:h-[500px] mx-auto my-5"
+                            // @ts-ignore
+                            allowfullscreen
+                        ></iframe>
+                    ))}
+                </div>
 
-    notFound();
-  }
+                <Gallery title={project.title} images={project.images} />
+            </div>
+        );
+    } catch (error) {
+        console.log(error);
+        console.log(
+            `/moderator/${MODERATOR_ID}/projects/${params.slug}/increment`
+        );
+
+        notFound();
+    }
 }
--- app/services/index.ts
+++ app/services/index.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://195.161.68.240:1000/",
+    // baseURL: "http://195.161.68.240:1000/",
+    baseURL: "http://127.0.0.1:8000/",
 });
+
+// /moderator/1/projects/1/increment
