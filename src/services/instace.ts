7e2db78a036d5a603ab764952d16c36734a6b90e
Muhiddin <88494561+Muhiddin0@users.noreply.github.com>
2025-01-19 22:12:43 +0500

    API manzilini o'zgartirish

--- src/services/instace.ts
+++ src/services/instace.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://api.themoviedb.org/3/"
+      ? "https://sys-partfolio-backend.6cpoqp.easypanel.host/api/"
       : "http://127.0.0.1:8081/api/",
   headers: {
     Authorization: "Token abd2499ed50789475a9ebb9fc613d9088f9bccd8",
